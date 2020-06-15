099136a1f510dc6d8faf7c2b8d0dfc9513e4ece8
wcalisto debian.home <williamcal.w@gmail.com>
2020-06-14 23:05:56 -0300

    Factorial argv

--- Recursiveness/factorial.c
+++ Recursiveness/factorial.c
@@ -3,15 +3,19 @@
 int		factorial(int f)
 {
 	if ( f == 0)
+	{
 		return (1);
+	}
 	else
+	{
 		return(f * factorial(f - 1));
+	}
 }
 
 int		main(int argc, char **argv)
 {
-		int i;
-		i = argv[1] - '0';
-		printf("Fatorial de %d = %d \n", argv[1], factorial(i);
-	
+		(void) argc;
+		
+		int i = argv[1] - '0';
+		printf("Fatorial de %d = %d \n", argv[1], factorial(i));
 }
