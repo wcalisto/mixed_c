736d2c18a77a5b2e92a5a727ce78c6f3ce6b0b29
wcalisto debian.home <williamcal.w@gmail.com>
2020-07-04 10:09:44 -0300

    ft_is_negative ternary operator test2

--- Strings/ft_is_negative.c
+++ Strings/ft_is_negative.c
@@ -1,14 +1,20 @@
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
 void	ft_is_negative(int n)
 {
 	char x; 
 	x = ( n >= 0 ) ? 'P' : 'N';
-	write (1 ,&x ,1 );
+	ft_putchar(x);
 }
 
 int		main(void)
 {
-	ft_is_negative(9);
+	ft_is_negative(-9);
+	ft_putchar('\n');
 	return (0);
 }
